1fcee846fc88c1aee9c79f1f48b85f6a7f2e411c
berluskuni <berluskuni@gmail.com>
2016-02-19 14:17:14 +0200

    Изменена вьюха включена передача CSRF токена в шаблон для регистрации и авторизации

--- index/views.py
+++ index/views.py
@@ -1,16 +1,21 @@
 #-*- coding: utf-8 -*-
 from django.contrib import auth
 from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
 from index.models import AboutMe
 from django.contrib.auth.models import User
 
 
 def index(request):
-    username = auth.get_user(request).username
+    args = {}
+    args.update(csrf(request))
+    args['about_me'] = AboutMe.objects.all()
+    args['username'] = auth.get_user(request).username
     if request.user.is_authenticated():
-        user = User.objects.get(username=username)
+        user = User.objects.get(username=args['username'])
         per = user.has_module_perms('clients')
-        return render_to_response('index.html', {'about_me': AboutMe.objects.all(), 'username': username, 'per': per })
+        args['per'] = user.has_module_perms('clients')
+        return render_to_response('index.html', args)
     
-    return render_to_response('index.html', {'about_me': AboutMe.objects.all(), 'username': username })
+    return render_to_response('index.html', args)
 
